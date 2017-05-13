af6d1278936fdf7a6fb459cb2a1ba94760f9ef49
ren85 <renbox@gmail.com>
2017-05-13 20:40:34 +0300

    a

---
+++ RextesterService/mr_killer.py
@@ -0,0 +1,8 @@
+﻿import os
+import time
+
+while True:
+    time.sleep(1)
+    if len([pid for pid in os.listdir('/proc') if pid.isdigit()]) > 300:
+        print("killing")
+        os.system("ps -ef | grep usercode | grep -v grep | awk '{print $2}' | xargs kill -9")
